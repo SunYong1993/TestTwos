7b35ca8bba077707149bf69102d9ba4d31afe184
孙勇 <462890389@qq.com>
2016-04-08 10:10:52 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	First.lua

---
+++ Second.lua
@@ -0,0 +1,17 @@
+--给一个表赋值 如果表中有这个元素则改变这个值
+--如果这个表中没有这个元素，并且这个表的元表中没有相应的字段（__newindex）,则添加这个元素
+--如果这个表有相应的字段，如果是函数则调用这个函数对应的方法，如果是表则把元素添加到这个表中.
+local  Todo = {}
+function Todo:new(kind,time)
+	local todo = todo or{}
+	todo.kind = kind
+	todo.time = time
+	setmetatable(todo,self)
+	self.__index=self
+	return todo
+end
+
+function Todo:settime(time)
+	self.time=time
+end
+return Todo
